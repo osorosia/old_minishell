178deb3bf07f99861a714f996b53c60883b116cf
osorosia <64416398+osorosia@users.noreply.github.com>
2022-01-17 05:53:00 +0000

    parser_testでlexerを使えるようにした

--- parser/parser_test.c
+++ parser/parser_test.c
@@ -1,9 +1,14 @@
 #include "../minishell.h"
 #include <assert.h>
 
+void test() {
+    t_token *tok = lexer("aaa");
+}
+
 int main(int argc, char **argv) {
     // automatic test
     setvbuf(stdout, NULL, _IONBF, 0);
+    test();
 
     printf("parser: OK\n");
 }
