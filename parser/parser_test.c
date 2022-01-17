a176c19eb3b1f183b44c9595e8ff9d3c831e262b
osorosia <64416398+osorosia@users.noreply.github.com>
2022-01-17 06:17:35 +0000

    add header

--- parser/parser_test.c
+++ parser/parser_test.c
@@ -1,8 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_test.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnishimo <rnishimo@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/17 06:15:30 by rnishimo          #+#    #+#             */
+/*   Updated: 2022/01/17 06:15:31 by rnishimo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../minishell.h"
 #include <assert.h>
 
 void test() {
     t_token *tok = lexer("aaa");
+    free_lexer(tok);
 }
 
 int main(int argc, char **argv) {
