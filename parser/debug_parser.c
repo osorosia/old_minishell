e1a740bd2ef8bccaaeba45c561d058b8fcf121cb
osorosia <64416398+osorosia@users.noreply.github.com>
2022-01-20 10:10:39 +0000

    debug関数の表示の修正

--- parser/debug_parser.c
+++ parser/debug_parser.c
@@ -6,7 +6,7 @@
 /*   By: rnishimo <rnishimo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 08:31:58 by rnishimo          #+#    #+#             */
-/*   Updated: 2022/01/20 09:10:03 by rnishimo         ###   ########.fr       */
+/*   Updated: 2022/01/20 10:09:14 by rnishimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,25 +35,30 @@ void debug_parser_recursive(t_node *node, long indent) {
         }
         printf("\n");
 
-        printf("%*sred_in : ", indent * INDENT_WIDTH, "");
-        t_node *redir_in = node->redir_in;
-        while (redir_in) {
-            printf("'%s' ", cmd->str);
-            redir_in = redir_in->next;
+        if (node->redir_in) {
+            printf("%*sred_in : ", indent * INDENT_WIDTH, "");
+            t_node *redir_in = node->redir_in;
+            while (redir_in) {
+                printf("'%s' ", cmd->str);
+                redir_in = redir_in->next;
+            }
+            printf("\n");
         }
-        printf("\n");
 
-        printf("%*sred_out: ", indent * INDENT_WIDTH, "");
-        t_node *redir_out = node->redir_out;
-        while (redir_out) {
-            printf("'%s' ", cmd->str);
-            redir_out = redir_out->next;
+        if (node->redir_out) {
+            printf("%*sred_out: ", indent * INDENT_WIDTH, "");
+            t_node *redir_out = node->redir_out;
+            while (redir_out) {
+                printf("'%s' ", cmd->str);
+                redir_out = redir_out->next;
+            }
+            printf("\n");
         }
-        printf("\n");
     }
 }
 
 void debug_parser(t_node *node) {
+    setvbuf(stdout, NULL, _IONBF, 0);
     printf("parser::::::::::::::\n");
     debug_parser_recursive(node, 1);
 }
