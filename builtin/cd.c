66d5c4eeea2946fa5ccf0adb533d79281fd55cda
osorosia <64416398+osorosia@users.noreply.github.com>
2022-01-21 16:15:29 +0000

    不要な変数を削除

--- builtin/cd.c
+++ builtin/cd.c
@@ -6,14 +6,13 @@
 /*   By: rnishimo <rnishimo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 15:16:20 by rnishimo          #+#    #+#             */
-/*   Updated: 2022/01/21 15:51:59 by rnishimo         ###   ########.fr       */
+/*   Updated: 2022/01/21 16:11:09 by rnishimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
 void cd(t_minishell *ms, t_node *node) {
-    char buf[512];
     node = node->cmds->next;
     if (node->next != NULL) {
         fprintf(stderr, "bash: cd: too many argument\n");
