58d77012622c3813f65a41e4857d771accfe4cf5
osorosia <64416398+osorosia@users.noreply.github.com>
2022-01-21 15:53:49 +0000

    余分な出力を削除

--- builtin/cd.c
+++ builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: rnishimo <rnishimo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 15:16:20 by rnishimo          #+#    #+#             */
-/*   Updated: 2022/01/21 15:26:56 by rnishimo         ###   ########.fr       */
+/*   Updated: 2022/01/21 15:51:59 by rnishimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,5 @@ void cd(t_minishell *ms, t_node *node) {
         fprintf(stderr, "bash: cd: too many argument\n");
         return ;
     }
-    printf("exec cd\n");
-    printf("prev:%s\n", getcwd(buf, 512));
     chdir(node->str);
-    printf("now:%s\n", getcwd(buf, 512));
 }
