a564b9057c747f0c206cee1340235e6f13a68b34
osorosia <64416398+osorosia@users.noreply.github.com>
2022-01-25 09:50:54 +0000

    #7 `echo -n`オプション追加

--- builtin/echo.c
+++ builtin/echo.c
@@ -6,19 +6,32 @@
 /*   By: rnishimo <rnishimo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 01:36:44 by rnishimo          #+#    #+#             */
-/*   Updated: 2022/01/22 01:39:57 by rnishimo         ###   ########.fr       */
+/*   Updated: 2022/01/25 09:47:15 by rnishimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
 void echo(t_minishell *ms, t_node *node) {
+    bool n_op = false;
     node = node->cmds->next;
+
+    if (node->str[0] == '-' && node->str[1] == 'n') {
+        long i = 2;
+        while (node->str[i] == 'n')
+            i++;
+        if (node->str[i] == '\0') {
+            n_op = true;
+            node = node->next;
+        }
+    }
+
     while (node != NULL) {
         printf("%s", node->str);
         node = node->next;
         if (node != NULL)
             printf(" ");
     }
-    printf("\n");
+    if (!n_op)
+        printf("\n");
 }
